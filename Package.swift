fdf43496245a088a007803f6bb71ce25d935a9bb
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2023-03-17 13:04:35 +0100

    mac os fix

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "UIComponents",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v11)],
     products: [
         .library(name: "UIComponents", targets: ["UIComponents"])
     ],
